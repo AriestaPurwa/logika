e170d299ddb35b19db8338d6aafee488c809c4b9
AriestaPurwa <ariestapurwanugraha55@gmail.com>
2022-04-12 11:37:35 +0700

    first commit

---
+++ 2.php
@@ -0,0 +1,9 @@
+<?php
+
+$bil=10; 
+
+if ($bil % 2 == 0){ 
+    echo "$bil Merupakan Bilangan Genap"; 
+}else {
+    echo "$bil Merupakan Bilangan Ganjil"; 
+?>
---
+++ 3.php
@@ -0,0 +1,6 @@
+<?php
+    $nilai        =array(1,3,8);
+    $jml_nilai    =array_sum($nilai);
+    
+    echo "jumlahnya ".$jml_nilai;
+?>
